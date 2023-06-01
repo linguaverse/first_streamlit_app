83249e2fc4426845ba7fc798d797d2f3983cda34
linguaverse <121514240+linguaverse@users.noreply.github.com>
2023-06-01 12:43:16 +0100

    Update streamlit_app.py
    removed default

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,14 +27,13 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header('Fruityice Fruit Advice!')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
-    streamlit.write('The user entered', fruit_choice)
 
 except URLError as e:
   streamlit.error()
