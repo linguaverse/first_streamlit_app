73f904ee7865b5795add916975c8c346dc70927d
linguaverse <121514240+linguaverse@users.noreply.github.com>
2023-06-01 09:02:23 +0100

    Update streamlit_app.py
    normalized json output

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,6 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# normalizing json version of response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
